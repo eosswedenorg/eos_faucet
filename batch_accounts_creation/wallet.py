f1740b0838419a654bdaeca5657a3291dc4aa4bd
Henrik Hautakoski <henrik@eossweden.org>
2019-11-15 12:46:10 +0100

    Remove the batch account creation code. we don't need it in here.

--- batch_accounts_creation/wallet.py
+++
@@ -1,9 +0,0 @@
-
-# paste your testnet account (to create accounts, transfer tokens) name
-ACCOUNT = ""
-
-# paste your local wallet name
-NAME = ""
-
-# paste your local wallet password
-PASSWD = ""
