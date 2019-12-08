d019dcdc006a99351aed73f6e860fe42158d0e65
Henrik Hautakoski <henrik@eossweden.org>
2019-12-08 22:04:09 +0100

    adding format_timespan() in helpers.py

---
+++ helpers.py
@@ -0,0 +1,16 @@
+
+def format_plural(num, unit, prefix='s') :
+    if num > 1 :
+        unit += prefix
+    return '{} {}'.format(num, unit)
+
+def format_timespan(s) :
+  m, s = divmod(s, 60)
+  h, m = divmod(m + (s > 0), 60) # Round up to nearest minute (becuse we don't show seconds)
+  parts = []
+
+  if h > 0 :
+    parts.append(format_plural(h, "hour"))
+  if m > 0 :
+    parts.append(format_plural(m, "minute"))
+  return ', '.join(parts)
